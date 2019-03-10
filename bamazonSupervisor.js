c1f177885c3414c8fad7d7bf592f6d2d6639e145
Randolph Bailey <randolphbailey@gmail.com>
2019-03-10 10:16:50 -0500

    First part of challenge 3 done

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -31,10 +31,23 @@ inquirer
   .then(answers => {
       switch(answers.command) {
         case 0:
-            view();
+            departmentSales();
             break;
         case 1:
-            low();
+            newDepartment();
             break;
       }
   });
+
+  function departmentSales() {
+    var query = "SELECT departments.`department_id`, products.`department_name`, departments.`over_head_costs`, SUM(products.`product_sales`) AS department_sales, (SUM(products.`product_sales`) - departments.`over_head_costs`) AS total_profit FROM products RIGHT JOIN departments ON products.`department_name` = departments.`department_name` GROUP BY departments.`department_id`";
+    con.query(query, function (err, result) {
+        if (err) throw err;
+        var data = [["Department ID", "Department Name", "Overhead Costs", "Product Sales", "Total Profit"]];
+        for (let i=0; i < result.length; i++) {
+            let itemRow = [result[i].department_id, result[i].department_name, result[i].over_head_costs, result[i].department_sales, result[i].total_profit];
+            data.push(itemRow);
+        }
+        console.log(table.table(data));
+    });
+  }
